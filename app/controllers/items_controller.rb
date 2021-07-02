673d8165868628fe21ebaeef72a6a3657900b722
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-07-02 16:28:49 +0900

    action 'order' create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: :order
   
   def index
     @items = Item.all
   end
+
+  def order
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+  
 end
